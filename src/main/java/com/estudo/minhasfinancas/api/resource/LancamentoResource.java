c0062cc0b5430d856731ee7de25f9ec11856a809
Renato Romão <renato.romao@eginfo.com.br>
2021-08-25 21:14:06 -0300

    ajustes

--- src/main/java/com/estudo/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/estudo/minhasfinancas/api/resource/LancamentoResource.java
@@ -45,6 +45,13 @@ public class LancamentoResource {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento( @PathVariable("id") long id) {
+		return service.obterPorId(id)
+				.map( lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK) )
+				.orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
 
 	@PutMapping("{id}")
 	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto) {
@@ -109,6 +116,19 @@ public class LancamentoResource {
 
 		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
 		return ResponseEntity.ok(lancamentos);
+	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+					.id(lancamento.getId())
+					.descricao(lancamento.getDescricao())
+					.valor(lancamento.getValor())
+					.mes(lancamento.getMes())
+					.ano(lancamento.getAno())
+					.status(lancamento.getStatus().name())
+					.tipo(lancamento.getTipo().name())
+					.usuario(lancamento.getUsuario().getId())
+					.build();
 	}
 
 	private Lancamento converter(LancamentoDTO dto) {
